b24a41fc1e63e2578691de84b32081d56eb45548
Inesh-Reddy <ineshreddy007@gmail.com>
2025-01-06 15:14:09 +0530

    Completed implementation of Api-fetcher-app

--- 04-Promised-based-api-fetcher-app/src/index.js
+++ 04-Promised-based-api-fetcher-app/src/index.js
@@ -4,6 +4,8 @@ const getDataFromAPIUsingPromises = async (url, todoId) => {
     return new Promise((resolve, reject) => {
         fetch(`${url}${todoId}`).then((res)=>{
             res.json().then((data)=>{
+                document.getElementById("result-title").innerHTML = data.title;
+                document.getElementById("result-completed").innerHTML = data.completed;
                 console.log(data);
             })
         })
@@ -13,8 +15,13 @@ const getDataFromAPIUsingPromises = async (url, todoId) => {
 const getDataFromApi = async (url, todoId) => {
     const response = await fetch(`${url}${todoId}`);
     const data = await response.json();
+    document.getElementById("result-title").innerText = data.title;
+    document.getElementById("result-completed").innerText = data.completed;
     console.log(data);
 }
 
-getDataFromApi(url1,1);
-getDataFromAPIUsingPromises(url1,1);
+
+
+
+
+
